2ef827253e23ce9b4db6bf332dda401d18015663
Hilguener <Hilgueneer@gmail.com>
2024-07-10 16:30:22 -0300

    feat: change some color properties

--- app/src/main/java/com/hilguener/marvelsuperheroes/ui/theme/Type.kt
+++ app/src/main/java/com/hilguener/marvelsuperheroes/ui/theme/Type.kt
@@ -1,9 +1,45 @@
-package com.example.ui.theme
+package com.hilguener.marvelsuperheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hilguener.marvelsuperheroes.R
 
-val AppTypography = Typography()
+val Poppins =
+    FontFamily(
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_black, FontWeight.Normal),
+        Font(R.font.poppins_medium, FontWeight.Normal),
+        Font(R.font.poppins_bold, FontWeight.Bold),
+    )
+val Typography = Typography(
+    bodyLarge =
+    TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+    ),
+    displayLarge =
+    TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp,
+    ),
+    displayMedium =
+    TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+    ),
+    displaySmall =
+    TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+    ),
+)
