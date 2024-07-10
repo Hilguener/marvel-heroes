127b209e68d9fee1cebfc45714850013145e3838
Hilguener <Hilgueneer@gmail.com>
2024-07-10 16:35:19 -0300

    feat: add LoadingButton composable

---
+++ app/src/main/java/com/hilguener/marvelsuperheroes/presentation/sign_in/LoadingButton.kt
@@ -0,0 +1,41 @@
+package com.hilguener.marvelsuperheroes.presentation.sign_in
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoadingButton(
+    text: String,
+    onClick: () -> Unit,
+    isLoading: Boolean,
+    modifier: Modifier = Modifier,
+    shape: androidx.compose.ui.graphics.Shape = RoundedCornerShape(10.dp)
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape,
+        colors = ButtonDefaults.buttonColors(Color.Red)
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator(color = Color.White, modifier = Modifier.size(25.dp))
+        } else {
+            Text(
+                text = text,
+                fontSize = 20.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
